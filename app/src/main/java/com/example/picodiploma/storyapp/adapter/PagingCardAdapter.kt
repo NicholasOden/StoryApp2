da925f15a9d202b8a4e731440cfe15ebe7a66bf2
NicholasOden <aoronmaster@gmail.com>
2023-05-29 09:25:13 +0700

    Adding DetaiLActivity Intent and FAB on activity_paging_list.xml

--- app/src/main/java/com/example/picodiploma/storyapp/adapter/PagingCardAdapter.kt
+++ app/src/main/java/com/example/picodiploma/storyapp/adapter/PagingCardAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.picodiploma.storyapp.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.picodiploma.storyapp.R
 import com.example.picodiploma.storyapp.data.response.Story
+import com.example.picodiploma.storyapp.view.DetailActivity
 
 
 class PagingCardAdapter : PagingDataAdapter<Story, PagingCardAdapter.PagingCardViewHolder>(
@@ -19,25 +21,28 @@ class PagingCardAdapter : PagingDataAdapter<Story, PagingCardAdapter.PagingCardV
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagingCardViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_paging_card, parent, false)
-        return PagingCardViewHolder(view)
+        return PagingCardViewHolder(view) { story: Story ->
+            val intent = Intent(parent.context, DetailActivity::class.java).apply {
+                putExtra("STORY_ID", story.id)
+            }
+            parent.context.startActivity(intent)
+        }
     }
 
     override fun onBindViewHolder(holder: PagingCardViewHolder, position: Int) {
         val story = getItem(position)
-        if (story != null) {
-            holder.bind(story)
-        }
+        story?.let { holder.bind(it) }
     }
 
-    class PagingCardViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class PagingCardViewHolder(view: View, private val listener: (Story) -> Unit) : RecyclerView.ViewHolder(view) {
         private val imageViewMain: ImageView = view.findViewById(R.id.pagingImageView)
         private val textViewTitleMain: TextView = view.findViewById(R.id.pagingTitleTextView)
         private val textViewDescriptionMain: TextView = view.findViewById(R.id.pagingDescriptionTextView)
 
         fun bind(story: Story) {
+            itemView.setOnClickListener { listener(story) } // <--- Set OnClickListener here
             textViewTitleMain.text = story.name
             textViewDescriptionMain.text = story.description
-            // Load the image using Glide or any other image loading library
             Glide.with(itemView.context)
                 .load(story.photoUrl)
                 .placeholder(R.drawable.logo)
@@ -45,6 +50,7 @@ class PagingCardAdapter : PagingDataAdapter<Story, PagingCardAdapter.PagingCardV
                 .into(imageViewMain)
         }
     }
+
 
     companion object {
         val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Story>() {
@@ -58,4 +64,5 @@ class PagingCardAdapter : PagingDataAdapter<Story, PagingCardAdapter.PagingCardV
         }
     }
 }
+
 
--- app/src/main/java/com/example/picodiploma/storyapp/view/paginglist/PagingListActivity.kt
+++ app/src/main/java/com/example/picodiploma/storyapp/view/paginglist/PagingListActivity.kt
@@ -1,14 +1,19 @@
 package com.example.picodiploma.storyapp.view.paginglist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.picodiploma.storyapp.adapter.PagingCardAdapter
 import com.example.picodiploma.storyapp.data.ApiServiceHelper
 import com.example.picodiploma.storyapp.data.PagingListRepository
 import com.example.picodiploma.storyapp.databinding.ActivityPagingListBinding
+import com.example.picodiploma.storyapp.view.CreateStoryActivity
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -27,7 +32,7 @@ class PagingListActivity : AppCompatActivity() {
         apiServiceHelper = ApiServiceHelper(getToken())
 
         val repository = PagingListRepository(apiServiceHelper)
-        viewModel = ViewModelProvider(this, ViewModelFactory(repository)).get(PagingListViewModel::class.java)
+        viewModel = ViewModelProvider(this, ViewModelFactory(repository))[PagingListViewModel::class.java]
 
         adapter = PagingCardAdapter()
 
@@ -37,6 +42,30 @@ class PagingListActivity : AppCompatActivity() {
         lifecycleScope.launch {
             viewModel.getStories().collectLatest { pagingData ->
                 adapter.submitData(pagingData)
+            }
+        }
+
+        // Set an OnClickListener for the FAB
+        binding.fabCreateStory.setOnClickListener {
+            // Open the create story activity
+            val intent = Intent(this, CreateStoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        adapter.addLoadStateListener { loadState ->
+            // Show loading spinner during initial load or refresh.
+            binding.progressBar.visibility =
+                if (loadState.refresh is LoadState.Loading) View.VISIBLE else View.GONE
+
+            // Show error state if the load fails.
+            if (loadState.refresh is LoadState.Error) {
+                Toast.makeText(this, "\uD83D\uDE28 Wooops ${(loadState.refresh as LoadState.Error).error}", Toast.LENGTH_LONG).show()
+            }
+
+            val isEmptyList = loadState.refresh is LoadState.NotLoading && adapter.itemCount == 0
+            // Show empty content message when list is empty.
+            if (isEmptyList) {
+                Toast.makeText(this, "List is empty", Toast.LENGTH_LONG).show()
             }
         }
     }
